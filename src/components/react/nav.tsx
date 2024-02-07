01dbbc0892bb2858c68fa29d7819110be28d8650
Kyle Van Den Hoff <kpvdh1999@gmail.com>
2024-02-07 21:06:16 +1100

    Font changes

--- src/components/react/nav.tsx
+++ src/components/react/nav.tsx
@@ -1,6 +1,6 @@
 export default function Nav() {
     return (<nav className="w-full flex flex-nowrap p-2 fixed top-0 z-10">
-        <a href="/" className="self-start font-dancing text-2xl font-bold text-black">Kyle & Jasmine</a>
+        <a href="/" className="self-start font-dancing text-2xl text-black">Kyle & Jasmine</a>
         <a href="/venue" className="ml-auto">Venue</a>
         <a href="/program" className="ml-auto">Program</a>
     </nav>)
