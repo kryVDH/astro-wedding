8083cc66e4d7108e0deae5a149c7b4eae9bb9a76
Kyle Van Den Hoff <kpvdh1999@gmail.com>
2024-02-11 10:43:16 +1100

    Nav improvements

--- src/components/react/nav.tsx
+++ src/components/react/nav.tsx
@@ -1,7 +1,9 @@
 export default function Nav() {
-    return (<nav className="w-full flex flex-nowrap p-2 fixed top-0 z-10">
-        <a href="/" className="self-start font-dancing text-2xl text-black">Kyle & Jasmine</a>
-        <a href="/venue" className="ml-auto">Venue</a>
-        <a href="/program" className="ml-auto">Program</a>
+    return (<nav className="w-full fixed top-0 z-50">
+        <ul className="list-none flex flex-nowrap p-2 items-center gap-4 px-4">
+            <li className="self-start font-dancing text-2xl text-black"><a href="/">Kyle & Jasmine</a></li>
+            <li className="ml-auto"><a href="/venue">VENUE</a></li>
+            <li><a href="/program">PROGRAM</a></li>
+        </ul>
     </nav>)
 }
