d5e442039faeb213d05edf70afe8cf1d4544556a
Kyle Van Den Hoff <kpvdh1999@gmail.com>
2024-02-11 12:16:43 +1100

    Add rsvp page

--- src/components/react/nav.tsx
+++ src/components/react/nav.tsx
@@ -4,6 +4,7 @@ export default function Nav() {
             <li className="self-start font-dancing text-2xl text-black"><a href="/">Kyle & Jasmine</a></li>
             <li className="ml-auto"><a href="/venue">VENUE</a></li>
             <li><a href="/program">PROGRAM</a></li>
+            <li><a href="/rsvp">RSVP</a></li>
         </ul>
     </nav>)
 }
