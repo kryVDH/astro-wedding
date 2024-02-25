6b08b9042cd2813df0c9ad32772ad9f3c699158e
Kyle Van Den Hoff <kpvdh1999@gmail.com>
2024-02-25 17:48:37 +1100

    Add links to nav

--- src/components/react/nav.tsx
+++ src/components/react/nav.tsx
@@ -20,9 +20,10 @@ const Nav = (props: any) => {
             <div className={"transition-all h-screen w-full md:w-1/2 z-40 absolute top-0 left-0 wed-bg-tan" + (!isOpen ? " -translate-x-full" : " ")} style={{boxShadow:  "0px 3px 6px #555"}}>
                 <ul className="list-none flex flex-col flex-nowrap p-2 items-center gap-4 px-4">
                     <li className="font-dancing text-2xl text-black whitespace-nowrap"><a href="/">Kyle & Jasmine</a></li>
+                    <li><a href="/rsvp">RSVP</a></li>
                     <li><a href="/venue">VENUE</a></li>
                     <li><a href="/program">PROGRAM</a></li>
-                    <li><a href="/rsvp">RSVP</a></li>
+                    <li><a href="/hotels">HOTELS</a></li>
                     <li><a href="/faq">FAQ</a></li>
                 </ul>
             </div>
@@ -30,9 +31,10 @@ const Nav = (props: any) => {
         <nav className="hidden lg:block w-full fixed top-0 z-50">
             <ul className="list-none flex flex-nowrap p-2 items-center gap-4 px-4">
                 <li className="self-start font-dancing text-2xl text-black whitespace-nowrap"><a href="/">Kyle & Jasmine</a></li>
-                <li className="ml-auto"><a href="/venue">VENUE</a></li>
+                <li className="ml-auto"><a href="/rsvp">RSVP</a></li>
+                <li><a href="/venue">VENUE</a></li>
                 <li><a href="/program">PROGRAM</a></li>
-                <li><a href="/rsvp">RSVP</a></li>
+                <li><a href="/hotels">HOTELS</a></li>
                 <li><a href="/faq">FAQ</a></li>
             </ul>
         </nav>
