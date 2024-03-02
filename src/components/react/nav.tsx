7bb33f13926fc1d50e33cbc40de38aec4b57d784
Kyle Van Den Hoff <kpvdh1999@gmail.com>
2024-03-02 12:50:52 +1100

    Style improvements

--- src/components/react/nav.tsx
+++ src/components/react/nav.tsx
@@ -1,4 +1,5 @@
 import { Children, useState, type ReactPropTypes } from "react";
+import logo from '../../photos/logo.png';
 
 const Nav = (props: any) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -7,19 +8,25 @@ const Nav = (props: any) => {
         setIsOpen(!isOpen);
     }
 
+    console.log(props);
+
     return (
     <>
         <nav className={"transition-all block lg:hidden w-full fixed top-0 z-30" + (isOpen ? " open" : " closed")} style={{boxShadow:  "0px 3px 6px #555"}}>
-            <ul className="list-none flex flex-nowrap p-2 items-center gap-4 px-4">
+            <ul className="list-none flex flex-nowrap p-2 items-center gap-2 px-4">
                 <li><button className="relative h-10 w-10 z-50" onClick={toggleOpen}>
                     { props.children }
                 </button></li>
-                <li className="font-dancing text-2xl text-black whitespace-nowrap"><a href="/">Kyle & Jasmine</a></li>
-                {props.backButtonVisible && <li className="ml-auto"><a className="ml-auto rounded-lg rsvp-btn font-bold text-black rsvp-btn-small" href="/rsvp">RSVP</a></li>}
+                <li className="font-dancing text-2xl text-black whitespace-nowrap">
+                    <a className="flex items-center gap-2" href="/"><img src={logo.src} width={30}></img> Kyle & Jasmine</a>
+                </li>
+                <li className="ml-auto"><a className="ml-auto rounded-lg rsvp-btn font-bold text-black rsvp-btn-small" href="/rsvp">RSVP</a></li> 
             </ul>
             <div className={"transition-all h-screen w-full md:w-1/2 z-40 absolute top-0 left-0 wed-bg-tan" + (!isOpen ? " -translate-x-full" : " ")} style={{boxShadow:  "0px 3px 6px #555"}}>
-                <ul className="list-none flex flex-col flex-nowrap p-2 items-center gap-4 px-4">
-                    <li className="font-dancing text-2xl text-black whitespace-nowrap"><a href="/">Kyle & Jasmine</a></li>
+                <ul className="list-none flex flex-col flex-nowrap p-2 items-center gap-2 px-4">
+                    <li className="font-dancing text-2xl text-black whitespace-nowrap">  
+                        <a className="flex items-center gap-2" href="/"><img src={logo.src} width={30}></img> Kyle & Jasmine</a>
+                    </li>
                     <li><a href="/rsvp">RSVP</a></li>
                     <li><a href="/venue">VENUE</a></li>
                     <li><a href="/program">PROGRAM</a></li>
